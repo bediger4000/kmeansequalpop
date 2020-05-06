d2f56bfcbcbfdaebbef2c87e94a9a01b6d0bfd41
Bruce Ediger <bediger8@gmail.com>
2020-05-05 20:09:58 -0600

    Add Assigned bool to allow cluster assignment

--- data/types.go
+++ data/types.go
@@ -2,7 +2,8 @@ package data
 
 // Point an x,y cartesian point
 type Point struct {
-	Pop float64
-	X   float64
-	Y   float64
+	Pop      float64
+	X        float64
+	Y        float64
+	Assigned bool
 }
