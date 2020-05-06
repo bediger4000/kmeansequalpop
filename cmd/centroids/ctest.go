1ab381b06ba53cf2bb15831ac3849de1c8d73c7b
Bruce Ediger <bediger8@gmail.com>
2020-05-05 20:11:04 -0600

    Add cluster initilaization

--- cmd/centroids/ctest.go
+++ cmd/centroids/ctest.go
@@ -29,11 +29,35 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano() + int64(os.Getpid()))
 
-	centroids := centroids.KmeansPPCentroids(points, k)
+	centers := centroids.KmeansPPCentroids(points, k)
 
-	fmt.Printf("# Found %d initial k-means++ centroids\n", len(centroids))
+	fmt.Printf("# Found %d initial k-means++ centroids\n", len(centers))
 
-	for i := range centroids {
-		fmt.Printf("# Centroid %d at (%f,%f)\n", i, centroids[i].X, centroids[i].Y)
+	for i := range centers {
+		fmt.Printf("# Centroid %d at (%f,%f)\n", i, centers[i].X, centers[i].Y)
+		fmt.Printf("%f %f c\n", centers[i].X, centers[i].Y)
+	}
+
+	clusters := centroids.InitialAssign(points, centers, k)
+
+	fmt.Printf("# Assigned points to %d clusters\n", len(clusters))
+	totalPopulation := 0.0
+	totalPoints := 0
+	for i, cluster := range clusters {
+		population := 0.0
+		totalPoints += len(cluster)
+		for i := range cluster {
+			population += cluster[i].Pop
+			totalPopulation += cluster[i].Pop
+		}
+		fmt.Printf("# cluster %d, %d items, %.0f population\n", i, len(cluster), population)
+	}
+	fmt.Printf("# %d total points, %f total population\n", totalPoints, totalPopulation)
+
+	for i := range clusters {
+		for j := range clusters[i] {
+			pt := clusters[i][j]
+			fmt.Printf("%f %f %.0f %d\n", pt.X, pt.Y, pt.Pop, i)
+		}
 	}
 }
