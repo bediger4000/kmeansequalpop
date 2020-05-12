9506f6407eca12a581034e184d31c8aabd74c73c
Bruce Ediger <bediger8@gmail.com>
2020-05-11 18:18:39 -0600

    Add fields to struct Point
    Assigned - to do the initial cluster assignment algorithm
    Xmoment, Ymoment - because the Point's (X,Y) never changes,
    nor does population
    CentroidX, CentroidY of the Point's current cluster's centroid

--- data/types.go
+++ data/types.go
@@ -2,8 +2,12 @@ package data
 
 // Point an x,y cartesian point
 type Point struct {
-	Pop      float64
-	X        float64
-	Y        float64
-	Assigned bool
+	Pop       float64
+	X         float64
+	Y         float64
+	Assigned  bool
+	Xmoment   float64
+	Ymoment   float64
+	CentroidX float64
+	CentroidY float64
 }
