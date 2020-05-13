d3a44b127ceb4d1aba66fd50dd4fcdcc7de10a4a
Bruce Ediger <bediger8@gmail.com>
2020-05-12 21:33:11 -0600

    Remove Centroid X,Y, add ClusterIdx
    This means that a Point can change clusters with
    a mere assignment, but you have to indirect through
    the ClusterIdx for other things.

--- data/types.go
+++ data/types.go
@@ -2,12 +2,11 @@ package data
 
 // Point an x,y cartesian point
 type Point struct {
-	Pop       float64
-	X         float64
-	Y         float64
-	Assigned  bool
-	Xmoment   float64
-	Ymoment   float64
-	CentroidX float64
-	CentroidY float64
+	Pop         float64
+	X           float64
+	Y           float64
+	Assigned    bool
+	Xmoment     float64
+	Ymoment     float64
+	CentroidIdx int // cluster membership
 }
